fed5d5d17056d2d5a155492e17b289fc9a23cd17
David Zhang <kiteabpl@outlook.com>
2020-10-30 17:35:32 +0800

    unpatch actuallfullscreen

--- dwm.c
+++ dwm.c
@@ -261,7 +261,6 @@ static void tagtoright(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
-static void togglefullscr(const Arg *arg);
 static void togglescratch(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
@@ -2208,13 +2207,6 @@ togglescratch(const Arg *arg)
 		}
 	} else
 		spawn(arg);
-}
-
-void
-togglefullscr(const Arg *arg)
-{
-  if(selmon->sel)
-    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
